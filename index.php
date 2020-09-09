4f4699251aed393b637fc3c0014278e93240b4dd
shango <2086751@talnet.nl>
2020-09-09 10:57:24 +0200

    new file toegevoegd

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project1</title>
+</head>
+<body>
+	<form action="action.php" method="post">
+ <input type="text" name="uname" placeholder="Username" required />
+ <input type="passowrd" name="pwd" placeholder="Password" required />
+ <input type="submit" /></br>
+ <a href="signup.php"> new user? Sign up!</a><br>
+ <a href="lostpwd.php"> Forgot password? 
+</form>
+
+</body>
+</html>
