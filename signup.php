215c414c694448913d94f8270c722e8685676181
shango <2086751@talnet.nl>
2020-09-09 11:19:06 +0200

    new files

--- signup.php
+++ signup.php
@@ -1,9 +1,18 @@
-<form action="signup.php" method="post">
- <input type="text" name="Fname" placeholder="FirstName" required /> <br>
- <input type="text" name="" placeholder=""  /> <br>
- <input type="text" name="Sname" placeholder="SecondName" required /> <br>
- <input type="text" name="Mail" placeholder="E-mail" required /> <br>
- <input type="text" name="Uname" placeholder="UserName" required /> <br>
- <input type="password" name="pwd" placeholder="Password" required /> <br>
- <input type="password" name="Rpwd" placeholder="Repeat Password" required /> <br>
- <input type="submit" /></br>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ <form action="signup.php" method="post">
+	 <input type="text" name="Fname" placeholder="FirstName" required /> <br>
+	 <input type="text" name="Insertn" placeholder="Insertion"  /> <br>
+	 <input type="text" name="Sname" placeholder="SecondName" required /> <br>
+	 <input type="text" name="Mail" placeholder="E-mail" required /> <br>
+	 <input type="text" name="Uname" placeholder="UserName" required /> <br>
+	 <input type="password" name="pwd" placeholder="Password" required /> <br>
+	 <input type="password" name="Rpwd" placeholder="Repeat Password" required /> <br>
+ 	 <input type="submit" /></br>
+ </form>
+</body>
+</html>
