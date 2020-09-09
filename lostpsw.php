d68b9fc991a23b984dc74b9f68904f1bccbae288
ShangoRG <48018150+ShangoRG@users.noreply.github.com>
2020-09-09 13:48:23 +0200

    Add files via upload

---
+++ lostpsw.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project1</title>
+</head>
+<body>
+	<form action="lostpsw.php" method="post">
+ <input type="text" name="uname" placeholder="Username" required />
+ <input type="passowrd" name="pwd" placeholder="Password" required />
+ <input type="submit" placeholder="submit" /></br>
