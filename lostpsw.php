5b478e7df04851a2f628c5994aa6d71f6b87c372
shango <2086751@talnet.nl>
2020-09-10 11:19:42 +0200

    lostpsw aangepast

--- lostpsw.php
+++ lostpsw.php
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
+
 <html>
 <head>
 	<title>Project1</title>
 </head>
 <body>
-	<form action="lostpsw.php" method="post">
+<form action="lostpsw.php" method="post">
  <input type="text" name="uname" placeholder="Username" required />
  <input type="passowrd" name="pwd" placeholder="Password" required />
-=======
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Project1</title>
-</head>
-<body>
-	<form action="lostpsw.php" method="post">
- <input type="text" name="uname" placeholder="Username" required />
- <input type="passowrd" name="pwd" placeholder="Password" required />
->>>>>>> d68b9fc991a23b984dc74b9f68904f1bccbae288
  <input type="submit" placeholder="submit" /></br>
+</form>
